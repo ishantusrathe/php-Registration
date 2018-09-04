028293e1259b3694dbc4a57c4eba854960b369d5
ishu <ishu233@hotmail.com>
2018-09-04 18:25:02 +0530

    update my reg.php

--- reg.php
+++ reg.php
@@ -33,7 +33,7 @@ $row=mysqli_num_rows($res);
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
+          <h4 class="modal-title">Select Categ..</h4>
         </div>
         <div class="modal-body">
 		<table class="border border-stripd">
